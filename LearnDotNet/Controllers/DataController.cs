a4845882b9eb104e78124c5b7126e1deabff6a27
HOYU <hoyu_z@163.com>
2017-09-28 17:12:08 +0800

    update

--- LearnDotNet/Controllers/DataController.cs
+++ LearnDotNet/Controllers/DataController.cs
@@ -18,30 +18,30 @@ namespace LearnDotNet.Controllers
 
 
         //上传文件，将文件放在指定文件夹下，计划做成工具类
-        // public async Task<IActionResult> Upload(List<IFormFile> files)
-        // {
-        //     long size = files.Sum(f => f.Length);
-        //     var filePath = Path.GetTempFileName();
-
-        //     foreach (var formFile in files)
-        //     {
-        //         if (formFile.Length > 0)
-        //         {
-        //             using (var stream = new FileStream(filePath, FileMode.Create))
-        //             {
-        //                 await formFile.CopyToAsync(stream);
-        //             }
-        //         }
-        //     }
-        //     return Ok(new { count = files.Count, size, filePath });
-        // }
-
-        public string Upload()
+        public async Task<IActionResult> Upload(List<IFormFile> files)
         {
-            var files = Request.Form.Files;
-
-            return "finish";
+            long size = files.Sum(f => f.Length);
+            var filePath = Path.GetTempFileName();
+            var file = Request.Form.Files;
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            return Ok(new { count = files.Count, size, filePath });
         }
+
+        // public string Upload()
+        // {
+        //     var files = Request.Form.Files;
+
+        //     return "finish";
+        // }
 
     }
 }
